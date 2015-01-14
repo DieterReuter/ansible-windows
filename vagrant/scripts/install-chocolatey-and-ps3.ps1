dba25d2d16643427debafc987d1efcffbb6f7d44
Stefan Scherer <scherer_stefan@icloud.com>
2015-01-14 21:26:28 +0100

    activate trial box

--- vagrant/scripts/install-chocolatey-and-ps3.ps1
+++ vagrant/scripts/install-chocolatey-and-ps3.ps1
@@ -1,3 +1,5 @@
+# activate the trial box
+Start-Process slmgr -ArgumentList "-ato"
 
 # Install latest Chocolatey
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
