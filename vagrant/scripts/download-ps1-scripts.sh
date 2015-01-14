54c8cb69da4c91615799c013380c5a6d3effd261
Dieter Reuter <dieter.reuter@me.com>
2015-01-14 16:52:24 +0100

    changed initial provision to install chocolatey and ps3

--- vagrant/scripts/download-ps1-scripts.sh
+++ vagrant/scripts/download-ps1-scripts.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 curl -O https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1
-curl -O https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/upgrade_to_ps3.ps1
-
---
+++ vagrant/scripts/install-chocolatey-and-ps3.ps1
@@ -0,0 +1,6 @@
+
+# Install latest Chocolatey
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+
+# Upgrade PowerShell
+cinst powershell
