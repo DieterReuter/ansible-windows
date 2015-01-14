882a2aa070ecc38ec4e4abdf8e9f90b0ea3209d6
Dieter Reuter <dieter.reuter@me.com>
2015-01-14 21:45:59 +0100

    install Chocolatey only, this includes PowerShell 4.0

--- vagrant/scripts/install-chocolatey.ps1
+++ vagrant/scripts/install-chocolatey.ps1
@@ -3,6 +3,3 @@ Start-Process slmgr -ArgumentList "-ato"
 
 # Install latest Chocolatey
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-
-# Upgrade to PowerShell 3.0
-choco install powershell -Version 3.0.20121027
