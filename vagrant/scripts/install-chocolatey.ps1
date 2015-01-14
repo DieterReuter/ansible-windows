dc051977221971c298bedda18b65cea0969071b8
Dieter Reuter <dieter.reuter@me.com>
2015-01-14 22:13:21 +0100

    install Chocolatey and PowerShell 4.0 plus reboot

--- vagrant/scripts/install-chocolatey.ps1
+++ vagrant/scripts/install-chocolatey.ps1
@@ -1,5 +1,9 @@
 # activate the trial box
 Start-Process slmgr -ArgumentList "-ato"
 
-# Install latest Chocolatey
+# install latest Chocolatey
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# install PowerShell 4.0 (this requires a reboot)
+choco install powershell
+shutdown /r /t 0
